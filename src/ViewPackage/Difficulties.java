b2ad02eadb890327c09aab9a7be3a9d46b38df37
Kareem Sabra <kareem20sabra@gmail.com>
2020-04-14 17:12:55 +0200

    Commit

--- src/ViewPackage/Difficulties.java
+++ src/ViewPackage/Difficulties.java
@@ -1,18 +1,35 @@
 package ViewPackage;
 
 import LogicPackage.Instantiation.PlayerSingleton;
+import MainPackage.ImportImage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Difficulties {
 
     public void prepareScene(Stage stage)
     {
+        StackPane stackPane = new StackPane();
+        stackPane.setPrefSize(1280,720);
+
+        try{
+            ImageView backGroundImage = new ImageView(new ImportImage().getImage("MenuBackground.jpg"));
+            backGroundImage.setPreserveRatio(true);
+            backGroundImage.setFitWidth(1280);
+            backGroundImage.setFitHeight(720);
+            stackPane.getChildren().add(backGroundImage);
+
+        }catch (Exception e){
+            System.out.println("Images error");
+        }
+
         Button beginnerDifficulty = new Button("Beginner");
         Button intermediateDifficulty = new Button("Intermediate");
         Button expertDifficulty = new Button("Expert");
@@ -41,10 +58,11 @@ public class Difficulties {
             }
         });
 
-        HBox box = new HBox(beginnerDifficulty, intermediateDifficulty, expertDifficulty);
-        box.setAlignment(Pos.CENTER);
-        box.setSpacing(40);
-        Scene scene = new Scene(box, 1280,720);
+        HBox buttonsBox = new HBox(beginnerDifficulty, intermediateDifficulty, expertDifficulty);
+        buttonsBox.setAlignment(Pos.CENTER);
+        buttonsBox.setSpacing(40);
+        stackPane.getChildren().add(buttonsBox);
+        Scene scene = new Scene(stackPane, 1280,720);
         stage.setScene(scene);
         stage.show();
     }
--- src/ViewPackage/GameMode.java
+++ src/ViewPackage/GameMode.java
@@ -1,13 +1,17 @@
 package ViewPackage;
 
 
+import MainPackage.ImportImage;
+import javafx.animation.RotateTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class GameMode {
@@ -16,6 +20,19 @@ public class GameMode {
 
 
     public void prepareScene(Stage stage){
+        StackPane stackPane = new StackPane();
+        stackPane.setPrefSize(1280,720);
+
+        try{
+            ImageView backGroundImage = new ImageView(new ImportImage().getImage("MenuBackground.jpg"));
+            backGroundImage.setPreserveRatio(true);
+            backGroundImage.setFitWidth(1280);
+            backGroundImage.setFitHeight(720);
+            stackPane.getChildren().add(backGroundImage);
+
+        }catch (Exception e){
+            System.out.println("Images error");
+        }
 
         Button arcade = new Button("Arcade");
         Button classic = new Button("Classic");
@@ -37,12 +54,13 @@ public class GameMode {
             }
         });
 
-        HBox box = new HBox();
-        box.getChildren().addAll(arcade,classic);
-        box.setSpacing(40);
-        box.setAlignment(Pos.CENTER);
+        HBox buttonsBox = new HBox();
+        buttonsBox.getChildren().addAll(arcade,classic);
+        buttonsBox.setSpacing(40);
+        buttonsBox.setAlignment(Pos.CENTER);
+        stackPane.getChildren().add(buttonsBox);
 
-        Scene scene = new Scene(box,1280,720);
+        Scene scene = new Scene(stackPane,1280,720);
 
         stage.setScene(scene);
         stage.show();
