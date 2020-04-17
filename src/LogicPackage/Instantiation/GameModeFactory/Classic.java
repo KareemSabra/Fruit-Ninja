4a1424445935c45f8972f4adc87c8cf7ba788807
Kareem Sabra <kareem20sabra@gmail.com>
2020-04-17 03:40:34 +0200

    Commit

--- src/LogicPackage/Instantiation/GameModeFactory/Classic.java
+++ src/LogicPackage/Instantiation/GameModeFactory/Classic.java
@@ -1,10 +1,20 @@
 package LogicPackage.Instantiation.GameModeFactory;
 
+import ViewPackage.ClassicScreen;
+import javafx.stage.Stage;
+
 public class Classic implements Mode {
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
 
     @Override
     public void gameMode()
     {
         System.out.println("Classic mode");
+        new ClassicScreen().prepareScene(stage);
     }
 }
--- src/ViewPackage/ClassicScreen.java
+++ src/ViewPackage/ClassicScreen.java
@@ -16,13 +16,16 @@ public class ClassicScreen extends GameScreen{
 
     public void prepareScene(Stage stage)
     {
-        super.prepareScene();
+        super.prepareScene(stage);
         Label livesLabel = new Label("X X X");
         livesLabel.setTextFill(Color.WHITE);
         VBox box2 = new VBox(livesLabel);
         box2.setAlignment(Pos.TOP_RIGHT);
 
-        Scene scene = new Scene(box2,1280,720);
+        super.mainPane.getChildren().add(box2);
+        super.mainPane.getChildren().add(super.pauseButtonBox);
+
+        Scene scene = new Scene(super.mainPane,1280,720);
         stage.setScene(scene);
         stage.show();
 
