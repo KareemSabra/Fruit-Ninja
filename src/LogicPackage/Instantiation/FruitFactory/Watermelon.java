c7c1a5d7feb824bda56788efa05b50878ec6f898
nouranHisham <63479155+nouranHisham@users.noreply.github.com>
2020-04-18 01:03:44 +0300

    Adding fruit factoery

---
+++ src/LogicPackage/Instantiation/FruitFactory/Watermelon.java
@@ -0,0 +1,9 @@
+package LogicPackage.Instantiation.FruitFactory;
+
+public class Watermelon implements Fruit {
+
+    @Override
+    public void fruitType() {
+        System.out.println("Easy Level");
+    }
+}
