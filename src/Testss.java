36af895b121d3df14291b5b036840d222039ebfb
Kareem Sabra <kareem20sabra@gmail.com>
2020-05-02 23:22:10 +0200

    Commit

--- src/Testss.java
+++ src/Testss.java
@@ -2,11 +2,8 @@ import LogicPackage.Commands.SaveGame;
 import LogicPackage.Commands.Invoker;
 
 public class Testss{
-
     public static void main(String[] args) {
-        Invoker invoker = new Invoker();
-        invoker.setCommands(new SaveGame());
-        invoker.execute();
+
     }
 
 }
